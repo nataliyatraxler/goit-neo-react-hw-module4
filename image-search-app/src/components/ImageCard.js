e5577687ae3fd44217665f7665b0e245309ff68f
nataliyatraxler <nataliya.traxler@gmail.com>
2024-12-18 20:42:26 +0100

    hw4

--- image-search-app/src/components/ImageCard.js
+++
@@ -1,9 +0,0 @@
-const ImageCard = ({ image }) => {
-  return (
-    <div>
-      <img src={image.urls.small} alt={image.alt_description} />
-    </div>
-  );
-};
-
-export default ImageCard;
--- image-search-app/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import { Oval } from 'react-loader-spinner';
-
-const Loader = () => (
-  <div>
-    <Oval color="blue" height={50} width={50} />
-  </div>
-);
-
-export default Loader;
